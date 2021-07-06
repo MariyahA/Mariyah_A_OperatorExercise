0f840cb1868fb3553cbfe60ce9ced3f53f4bc785
MariyahA <86676361+MariyahA@users.noreply.github.com>
2021-07-06 12:22:53 -0500

    Add files via upload

---
+++ Mariyah_A_OperatorExercise.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var Num1 = 262
+var Num2 = 56
+var add = Num1 + Num2
+var sub = Num1 - Num2
+var mult = Num1 * Num2
+var div = Num1 / Num2
+print("\(Num1) + \(Num2) = \(add)")
+print("\(Num1) - \(Num2) = \(sub)")
+print("\(Num1) * \(Num2) = \(mult)")
+print("\(Num1) / \(Num2) = \(div)")
